28cd3eb969ccb292f56070d9a5dcca65b7bf6376
easyflygit <akv888@inbox.ru>
2024-09-25 15:51:20 +0200

    добавил модели в models.py, настроил admin.py

--- mail_processor/admin.py
+++ mail_processor/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import EmailAccount, ParsingRule
 
-# Register your models here.
+
+@admin.register(EmailAccount)
+class EmailAccountAdmin(admin.ModelAdmin):
+    list_display = ('email', 'server', 'port')
+
+
+@admin.register(ParsingRule)
+class ParsingRuleAdmin(admin.ModelAdmin):
+    list_display = ('email_account', 'column_name', 'db_column')
+
--- mail_processor/models.py
+++ mail_processor/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class EmailAccount(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    server = models.CharField(max_length=255)
+    port = models.IntegerField()
+
+
+class ParsedFile(models.Model):
+    upload_date = models.DateTimeField(auto_now_add=True)
+    file_name = models.CharField(max_length=255)
+    data = models.JSONField()
+
+
+class ParsingRule(models.Model):
+    email_account = models.ForeignKey(EmailAccount,
+                                      related_name='parsing_rule',
+                                      on_delete=models.CASCADE)
+    column_name = models.CharField(max_length=255)
+    db_column = models.CharField(max_length=255)
